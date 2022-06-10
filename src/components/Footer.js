922504a11eb0c6fc072eea66648882d12b844b86
Halibal <halibal95@gmail.com>
2022-06-10 17:08:22 +0300

    todo project started

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p className='footer-info'>
+                Click to edit a todo <br />
+                Created by <a href="https://github.com/halibal">Halil ALGUL</a> <br />
+                Part of <a href="https://todomvc.com">TodoMVC</a>
+            </p>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <h1 className='todo-header'>todos</h1>
+    )
+}
+
+export default Header;
